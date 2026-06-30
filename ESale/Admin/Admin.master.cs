dae190895d242b5edd8bf63e0741399e0e8494b9
Syedbasha81421 <syeed8142@gmail.com>
2026-07-01 01:09:39 +0530

    commit

--- ESale/Admin/Admin.master.cs
+++ ESale/Admin/Admin.master.cs
@@ -85,7 +85,6 @@ public partial class Admin_Admin : System.Web.UI.MasterPage
 
     private void GenerateDynamicLeftForm()
     {
-
         objScreens = new object();
         objScreens = PGetAllScreens();
         dsScreens = (DataSet)objScreens;
@@ -93,43 +92,74 @@ public partial class Admin_Admin : System.Web.UI.MasterPage
         dsModule = new DataSet();
         dsModule = (DataSet)objModules;
         StringBuilder sb = new StringBuilder(string.Empty);
-        int i = 1;
 
-        //sb.Append("<ul id='menu'>");
-        sb.Append("<ul class='x-navigation' id='menu' >");
+        sb.Append("<div class='sidebar-header'>");
+        sb.Append("<div><img src='../assets/images/logo-icon.png' class='logo-icon' alt='logo icon'></div>");
+        sb.Append("<div><h4 class='logo-text'>Eyewa ERP</h4></div>");
+        sb.Append("<div class='toggle-icon ms-auto'><i class='bx bx-arrow-back'></i></div>");
+        sb.Append("</div>");
 
-        sb.Append(@"<li class='xn-logo'>
-                                <a href='../Admin/Default.aspx'></a>
-                                <a style='cursor:pointer;' class='x-navigation-control'></a>
-                            </li>");
+        sb.Append("<ul class='metismenu' id='menu'>");
+        
         foreach (DataRow drModule in dsModule.Tables[0].Rows)
         {
             string ModuleName = drModule["ModuleName"].ToString();
-            bool btest = true;
+            string iconClass = GetModuleIcon(ModuleName);
+            
+            bool hasScreens = false;
+            StringBuilder screensSb = new StringBuilder();
+            
             foreach (DataRow drRoleScreen in dsScreens.Tables[0].Rows)
             {
                 if (ModuleName == drRoleScreen["ModuleName"].ToString())
                 {
-                    if (btest)
-                    {
-                        sb.Append("<li class='xn-openable'>");
-                        sb.Append("<a href='#'> <span class='xn-text'>" + ModuleName + "</span></a> ");
-                        sb.Append("<ul><li><a href='" + drRoleScreen["SCREENURL"] + "' >");
-                        sb.Append(drRoleScreen["SCREENNAME"].ToString() + "<span></span></a></li>");
-                        btest = false;
-                        i++;
-                    }
-                    else
-                    {
-                        sb.Append("<li><a href='" + drRoleScreen["SCREENURL"] + "'>");
-                        sb.Append(drRoleScreen["SCREENNAME"].ToString() + "<span></span></a></li>");
-                    }
+                    hasScreens = true;
+                    screensSb.Append("<li><a href='" + drRoleScreen["SCREENURL"] + "'><i class='bx bx-radio-circle'></i>" + drRoleScreen["SCREENNAME"].ToString() + "</a></li>");
                 }
             }
-            sb.Append("</ul></li>");
+            
+            if (hasScreens)
+            {
+                sb.Append("<li>");
+                sb.Append("<a href='javascript:;' class='has-arrow'>");
+                sb.Append("<div class='parent-icon'><i class='" + iconClass + "'></i></div>");
+                sb.Append("<div class='menu-title'>" + ModuleName + "</div></a>");
+                sb.Append("<ul>" + screensSb.ToString() + "</ul>");
+                sb.Append("</li>");
+            }
+            else
+            {
+                sb.Append("<li>");
+                sb.Append("<a href='javascript:;'>");
+                sb.Append("<div class='parent-icon'><i class='" + iconClass + "'></i></div>");
+                sb.Append("<div class='menu-title'>" + ModuleName + "</div></a>");
+                sb.Append("</li>");
+            }
         }
         sb.Append("</ul>");
         colapsableMenuBlock.InnerHtml = sb.ToString();
+    }
+
+    private string GetModuleIcon(string moduleName)
+    {
+        moduleName = moduleName.ToLower();
+        if (moduleName.Contains("dashboard")) return "bx bx-home-alt";
+        if (moduleName.Contains("application")) return "bx bx-category";
+        if (moduleName.Contains("widget")) return "bx bx-cookie";
+        if (moduleName.Contains("ecommerce") || moduleName.Contains("sale")) return "bx bx-cart";
+        if (moduleName.Contains("component")) return "bx bx-bookmark-heart";
+        if (moduleName.Contains("content")) return "bx bx-repeat";
+        if (moduleName.Contains("icon")) return "bx bx-donate-blood";
+        if (moduleName.Contains("form")) return "bx bx-message-square-edit";
+        if (moduleName.Contains("table")) return "bx bx-grid-alt";
+        if (moduleName.Contains("auth") || moduleName.Contains("security")) return "bx bx-lock";
+        if (moduleName.Contains("user") || moduleName.Contains("profile")) return "bx bx-user-circle";
+        if (moduleName.Contains("chart")) return "bx bx-line-chart";
+        if (moduleName.Contains("map")) return "bx bx-map-alt";
+        if (moduleName.Contains("report")) return "bx bx-file";
+        if (moduleName.Contains("setting")) return "bx bx-cog";
+        
+        return "bx bx-grid-alt"; // Default icon
     }
 
     private object PGetAllScreens()
